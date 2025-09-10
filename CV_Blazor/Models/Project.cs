61695a7fcda7a524bf04d942fc846f2b1e1db87f
Máximo Brunetti <67173706+TuXimo@users.noreply.github.com>
2025-09-10 16:27:40 -0300

    pre skills

--- CV_Blazor/Models/Project.cs
+++ CV_Blazor/Models/Project.cs
@@ -5,8 +5,11 @@
         public string Title { get; set; }
         public string ShortDescription { get; set; }
         public string MainImage { get; set; }
-        public List<string> SecondaryImages { get; set; } = new();
+        public List<string> SecondaryImages { get; set; }
         public string ProjectLink { get; set; }
         public string GithubLink { get; set; }
+
+        public string Slug => Title?.ToLower().Replace(" ", "-").Replace(".", "").Replace(",", "") ?? "";
     }
+
 }

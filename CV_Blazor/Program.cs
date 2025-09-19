a66b8d9857fea9b0ebbc08e07597aee283018796
Máximo Brunetti <67173706+TuXimo@users.noreply.github.com>
2025-09-19 01:21:23 -0300

    xd

--- CV_Blazor/Program.cs
+++ CV_Blazor/Program.cs
@@ -2,16 +2,42 @@ using CV_Blazor;
 using CV_Blazor.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<PortfolioService>();
-builder.Services.AddScoped<SkillService>();
-
-Console.WriteLine("Hola amigo 2");
+using Microsoft.Extensions.Localization;
+using Microsoft.JSInterop;
+using System.Globalization;
 
 
-await builder.Build().RunAsync();
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+        builder.Services.AddScoped<PortfolioService>();
+        builder.Services.AddScoped<SkillService>();
+
+        builder.Services.AddLocalization();
+
+        var host = builder.Build();
+
+        const string defaultCulture = "us-EN";
+
+        var js = host.Services.GetRequiredService<IJSRuntime>();
+        var result = await js.InvokeAsync<string>("blazorCulture.get");
+        var culture = CultureInfo.GetCultureInfo(result ?? defaultCulture);
+
+        if (result == null)
+        {
+            await js.InvokeVoidAsync("blazorCulture.set", defaultCulture);
+        }
+
+        CultureInfo.DefaultThreadCurrentCulture = culture;
+        CultureInfo.DefaultThreadCurrentUICulture = culture;
+
+        await host.RunAsync();
+    }
+}

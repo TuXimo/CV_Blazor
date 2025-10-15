cdd6a1eeeef249e4f8d572dca2d22eebcb7d2e6a
Máximo Brunetti <67173706+TuXimo@users.noreply.github.com>
2025-10-15 15:53:54 -0300

    fix coso

--- CV_Blazor/Program.cs
+++ CV_Blazor/Program.cs
@@ -1,44 +1,44 @@
-using CV_Blazor;
-using CV_Blazor.Services;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Localization;
-using Microsoft.JSInterop;
-using System.Globalization;
-
-
-public class Program
+namespace CV_Blazor
 {
-    public static async Task Main(string[] args)
+    using Microsoft.AspNetCore.Components.Web;
+    using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+    using Microsoft.JSInterop;
+    using System.Globalization;
+    using Services;
+
+    public static class Program
     {
-        var builder = WebAssemblyHostBuilder.CreateDefault(args);
-        builder.RootComponents.Add<App>("#app");
-        builder.RootComponents.Add<HeadOutlet>("head::after");
-
-        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-        builder.Services.AddScoped<PortfolioService>();
-        builder.Services.AddScoped<SkillService>();
-        builder.Services.AddSingleton<LocalizationService>();
-
-        builder.Services.AddLocalization();
-
-        var host = builder.Build();
-
-        const string defaultCulture = "en-US";
-
-        var js = host.Services.GetRequiredService<IJSRuntime>();
-        var result = await js.InvokeAsync<string>("blazorCulture.get");
-        var culture = CultureInfo.GetCultureInfo(result ?? defaultCulture);
-
-        if (result == null)
+        public static async Task Main(string[] args)
         {
-            await js.InvokeVoidAsync("blazorCulture.set", defaultCulture);
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddScoped<PortfolioService>();
+            builder.Services.AddScoped<SkillService>();
+            builder.Services.AddSingleton<LocalizationService>();
+
+            builder.Services.AddLocalization();
+
+            var host = builder.Build();
+
+            const string defaultCulture = "en-US";
+
+            var js = host.Services.GetRequiredService<IJSRuntime>();
+            var result = await js.InvokeAsync<string>("blazorCulture.get");
+            var culture = CultureInfo.GetCultureInfo(result ?? defaultCulture);
+
+            if (result == null)
+            {
+                await js.InvokeVoidAsync("blazorCulture.set", defaultCulture);
+            }
+
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CultureInfo.DefaultThreadCurrentUICulture = culture;
+
+            await host.RunAsync();
         }
-
-        CultureInfo.DefaultThreadCurrentCulture = culture;
-        CultureInfo.DefaultThreadCurrentUICulture = culture;
-
-        await host.RunAsync();
     }
 }

af448f292724f9a35ea1cb5df919035a12093362
Máximo Brunetti <67173706+TuXimo@users.noreply.github.com>
2025-10-15 22:33:04 -0300

    GOD
    This reverts commit d2d73a8f19cacb24485d5f13fa0db6d724f8db0a.

--- CV_Blazor/wwwroot/js/site.js
+++ CV_Blazor/wwwroot/js/site.js
@@ -1,23 +1,19 @@
 ﻿window.toggleDarkMode = function (isDark) {
-    const root = document.documentElement; // <html>
     if (isDark) {
-        root.classList.add("dark-mode");
+        document.body.classList.add("dark-mode");
         localStorage.setItem("theme", "dark");
     } else {
-        root.classList.remove("dark-mode");
+        document.body.classList.remove("dark-mode");
         localStorage.setItem("theme", "light");
     }
 };
 
-
 // Al cargar, aplicar el tema guardado
 window.applySavedTheme = function () {
-    const root = document.documentElement;
     const savedTheme = localStorage.getItem("theme");
     if (savedTheme === "dark") {
-        root.classList.add("dark-mode");
+        document.body.classList.add("dark-mode");
         return true;
     }
     return false;
 };
-

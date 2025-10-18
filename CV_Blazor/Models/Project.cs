8cf3c628e9d3b2aa00594ea49f2649da4c90f790
Máximo Brunetti <67173706+TuXimo@users.noreply.github.com>
2025-10-17 22:18:48 -0300

    GOL

--- CV_Blazor/Models/Project.cs
+++ CV_Blazor/Models/Project.cs
@@ -8,6 +8,7 @@
         public List<string> SecondaryImages { get; set; } = new();
         public string ProjectLink { get; set; } = string.Empty;
         public string? GithubLink { get; set; }
+        public List<string> Technologies { get; set; } = new();
         public string Slug => Title?.ToLower().Replace(" ", "-").Replace(".", "").Replace(",", "") ?? "";
     }
 }
